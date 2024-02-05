ecb772fceb99d900de1299e1962b8a67f6d28f2c
dilanjan <dilanjan@zmessenger.com>
2024-02-05 10:24:35 +0530

    added places image padding

--- src/views/index-sections/BasicElements.js
+++ src/views/index-sections/BasicElements.js
@@ -32,7 +32,7 @@ function BasicElements() {
         <Container>
           {/* <Col className="ml-auto mr-auto"md="10" xl="6"> */}
           <h3 className="title">Places</h3>
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -41,8 +41,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Sri Dalada Maligawa</CardTitle>
               <CardText>
-              The Sri Dalada Maligawa, located in Kandy, Sri Lanka, 
-              houses the tooth relic of the revered Buddha, a tooth within the royal palace complex..
+                The Sri Dalada Maligawa, located in Kandy, Sri Lanka, houses the
+                tooth relic of the revered Buddha, a tooth within the royal
+                palace complex..
               </CardText>
               <Button
                 color="primary"
@@ -54,7 +55,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -63,8 +64,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Botanical Garden</CardTitle>
               <CardText>
-              The 59 hectare park is spread over 146 acres and is home to around 4,000 species of plants including 
-              medicinal trees, flowering trees and fascinating trails.
+                The 59 hectare park is spread over 146 acres and is home to
+                around 4,000 species of plants including medicinal trees,
+                flowering trees and fascinating trails.
               </CardText>
               <Button
                 color="primary"
@@ -76,7 +78,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -85,8 +87,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Bahirawakanda Temple</CardTitle>
               <CardText>
-              Visible from Kandy is the Bahiravakanda Temple, which is 2 km 
-              from the city center and has a huge Buddha statue at Dhyana Mudra.
+                Visible from Kandy is the Bahiravakanda Temple, which is 2 km
+                from the city center and has a huge Buddha statue at Dhyana
+                Mudra.
               </CardText>
               <Button
                 color="primary"
@@ -98,7 +101,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -107,8 +110,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Kandy View Point</CardTitle>
               <CardText>
-              It is located on Rajapihilla Mawatha at a height of 550 meters above sea level. 
-              It offers a panoramic view of Kandy Lake, Sri Dalada Palace, Kandy Royal Palace and the city center.
+                It is located on Rajapihilla Mawatha at a height of 550 meters
+                above sea level. It offers a panoramic view of Kandy Lake, Sri
+                Dalada Palace, Kandy Royal Palace and the city center.
               </CardText>
               <Button
                 color="primary"
@@ -120,7 +124,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -129,8 +133,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Royal Palace Park - Kandy</CardTitle>
               <CardText>
-              Wells Park, also known as Wells Park or Rajavasala Park, A very well-maintained 
-              park and woodland, originally laid out by Sri Wickrama Rajasinghe with good walking trails. 
+                Wells Park, also known as Wells Park or Rajavasala Park, A very
+                well-maintained park and woodland, originally laid out by Sri
+                Wickrama Rajasinghe with good walking trails.
               </CardText>
               <Button
                 color="primary"
@@ -142,7 +147,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -151,8 +156,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Garadi Ella</CardTitle>
               <CardText>
-              Located in the lush hills of Kandy district, Gerandi Falls is one of the most beautiful waterfalls in Sri Lanka, offering an adventurous journey 
-              through dense forests and rocky terrain. 
+                Located in the lush hills of Kandy district, Gerandi Falls is
+                one of the most beautiful waterfalls in Sri Lanka, offering an
+                adventurous journey through dense forests and rocky terrain.
               </CardText>
               <Button
                 color="primary"
@@ -164,19 +170,18 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
-              
               className="img-raised"
               src={require("assets/img/71.jpg")}
             ></CardImg>
             <CardBody>
               <CardTitle tag="h4">Yahangala Mountain</CardTitle>
               <CardText>
-              Steeped in Ramayana legend, Yahangala is the rock where King Ravana was laid to rest after he was killed by 
-              Lord Rama in the final battle. 'Yahangala' means bedrock in Sinhala.
+                Steeped in Ramayana legend, Yahangala is the rock where King
+                Ravana was laid to rest after he was killed by Lord Rama in the
+                final battle. 'Yahangala' means bedrock in Sinhala.
               </CardText>
               <Button
                 color="primary"
@@ -188,7 +193,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -197,8 +202,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Hanthana Mountain</CardTitle>
               <CardText>
-              The Hanthana Mountain Range lies in the central highlands of Sri Lanka, south-west of Kandy. 
-              The maximum height of the range is 1,200 m (3,800 ft). The mountain range consists of seven peaks.
+                The Hanthana Mountain Range lies in the central highlands of Sri
+                Lanka, south-west of Kandy. The maximum height of the range is
+                1,200 m (3,800 ft). The mountain range consists of seven peaks.
               </CardText>
               <Button
                 color="primary"
@@ -210,7 +216,7 @@ function BasicElements() {
             </CardBody>
           </Card>
 
-          <Card style={{ width: "20rem" }}>
+          <Card style={{ width: "20rem", margin: "25px" }}>
             <CardImg
               alt="..."
               className="img-raised"
@@ -219,8 +225,9 @@ function BasicElements() {
             <CardBody>
               <CardTitle tag="h4">Ambuluwawa</CardTitle>
               <CardText>
-              Ambuluwawa Tower is the first Multi-Religious Center in Sri Lanka. 
-              It is known that Ambuluwawa Temple is evocative of a Buddhist 'stupa' with a height of 48 meters.
+                Ambuluwawa Tower is the first Multi-Religious Center in Sri
+                Lanka. It is known that Ambuluwawa Temple is evocative of a
+                Buddhist 'stupa' with a height of 48 meters.
               </CardText>
               <Button
                 color="primary"
