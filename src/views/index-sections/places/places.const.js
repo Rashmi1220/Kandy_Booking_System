01c37a3d5c3a7fa798ade2e6b68f192b2db21dc5
dilanjan <dilanjan@zmessenger.com>
2024-06-19 10:33:07 +0530

    remove ex pages

--- src/views/index-sections/places/places.const.js
+++ src/views/index-sections/places/places.const.js
@@ -1,76 +1,75 @@
 export const placesList = [
-    {
-      title: "Sri Dalada Maligawa",
-      description: "The Sri Dalada Maligawa, located in Kandy, Sri Lanka, houses the tooth relic of the revered Buddha, a tooth within the royal palace complex.",
-      images: [require("assets/img/1.jpg")],
-      route: "/maligawa"
-    },
-    {
-      title: "Botanical Garden",
-      description: "The 59 hectare park is spread over 146 acres and is home to around 4,000 species of plants including medicinal trees, flowering trees and fascinating trails.",
-      images: [require("assets/img/2.jpg")],
-      route: "/garden"
-    },
-    {
-      title: "Bahirawakanda Temple",
-      description: "Visible from Kandy is the Bahiravakanda Temple, which is 2 km from the city center and has a huge Buddha statue at Dhyana Mudra.",
-      images: [require("assets/img/3.jpg")],
-      route: "/bahiravakanda"
-    },
-    {
-      title: "Kandy View Point",
-      description: "It is located on Rajapihilla Mawatha at a height of 550 meters above sea level. It offers a panoramic view of Kandy Lake, Sri Dalada Palace, Kandy Royal Palace and the city center.",
-      images: [require("assets/img/4.jpg")],
-      route: "/viewPoint"
-    },
-    {
-      title: "Royal Palace Park - Kandy",
-      description: "Wells Park, also known as Wells Park or Rajavasala Park, A very well-maintained park and woodland, originally laid out by Sri Wickrama Rajasinghe with good walking trails.",
-      images: [require("assets/img/5.jpg")],
-      route: "/royalPark",
-      caption:""
-    },
-    {
-      title: "Garadi Ella",
-      description: "Located in the lush hills of Kandy district, Gerandi Falls is one of the most beautiful waterfalls in Sri Lanka, offering an adventurous journey through dense forests and rocky terrain.",
-      images: [require("assets/img/61.jpg")],
-      route: "/garadiElla"
-    },
-    {
-      title: "Yahangala Mountain",
-      description: "Steeped in Ramayana legend, Yahangala is the rock where King Ravana was laid to rest after he was killed by Lord Rama in the final battle. 'Yahangala' means bedrock in Sinhala.",
-      images: [require("assets/img/71.jpg")],
-      route: "/yahangala"
-    },
-    {
-      title: "Hanthana Mountain",
-      description: "The Hanthana Mountain Range lies in the central highlands of Sri Lanka, south-west of Kandy. The maximum height of the range is 1,200 m (3,800 ft). The mountain range consists of seven peaks.",
-      images: [require("assets/img/10.jpg")],
-      route: "/hanthana"
-    },
-    {
-      title: "Ambuluwawa",
-      description: "Ambuluwawa Tower is the first Multi-Religious Center in Sri Lanka. It is known that Ambuluwawa Temple is evocative of a Buddhist 'stupa' with a height of 48 meters.",
-      images: [require("assets/img/11.jpg")],
-      route: "/ambuluwawa"
-    },
-    {
-      title: "Udawaththa Kale",
-      description: "Udawattakele Forest Reserve often spelled as Udawatta Kele, is a historic forest reserve on a hill-ridge in the city of Kandy. It is 104 hectares large. During the days of the Kandyan kingdom, Udawattakele was known as 'Uda Wasala Watta' in Sinhalese meaning 'the garden above the royal palace'.",
-      images: [require("assets/img/55.jpg")],
-      route: "/udawaththakale"
-    },
-    {
-      title: "Peradeniya Campus",
-      description: "The University covers about 700 hectares of land and the developed area covering approximately 130 hectares is occupied by buildings of the Faculties, Halls of Residence, Staff bungalows etc. Throughout the campus most trees begin to bear flowers in early March signaling the coming of spring.",
-      images: [require("assets/img/76.jpg")],
-      route: "/peradeniyacampus"
-    },
-    {
-      title: "Bellwood",
-      description: "This magnificent scenic spot is not far from the Kandy city limits and you can easily reach the place from Kandy city via Ampitiya town to Talatuoya and then to Bellwood area. This place is considered a camping ground and if you camp overnight near Muthu Kelina lake make sure to watch the sunrise.",
-      images: [require("assets/img/33.jpg")],
-      route: "/bellwood"
-    }
+  {
+    title: "Sri Dalada Maligawa",
+    description: "The Sri Dalada Maligawa, located in Kandy, Sri Lanka, houses the tooth relic of the revered Buddha, a tooth within the royal palace complex.",
+    images: [require("assets/img/1.jpg")],
+    route: "/maligawa"
+    // },
+    // {
+    //   title: "Botanical Garden",
+    //   description: "The 59 hectare park is spread over 146 acres and is home to around 4,000 species of plants including medicinal trees, flowering trees and fascinating trails.",
+    //   images: [require("assets/img/2.jpg")],
+    //   route: "/garden"
+    // },
+    // {
+    //   title: "Bahirawakanda Temple",
+    //   description: "Visible from Kandy is the Bahiravakanda Temple, which is 2 km from the city center and has a huge Buddha statue at Dhyana Mudra.",
+    //   images: [require("assets/img/3.jpg")],
+    //   route: "/bahiravakanda"
+    // },
+    // {
+    //   title: "Kandy View Point",
+    //   description: "It is located on Rajapihilla Mawatha at a height of 550 meters above sea level. It offers a panoramic view of Kandy Lake, Sri Dalada Palace, Kandy Royal Palace and the city center.",
+    //   images: [require("assets/img/4.jpg")],
+    //   route: "/viewPoint"
+    // },
+    // {
+    //   title: "Royal Palace Park - Kandy",
+    //   description: "Wells Park, also known as Wells Park or Rajavasala Park, A very well-maintained park and woodland, originally laid out by Sri Wickrama Rajasinghe with good walking trails.",
+    //   images: [require("assets/img/5.jpg")],
+    //   route: "/royalPark",
+    //   caption:""
+    // },
+    // {
+    //   title: "Garadi Ella",
+    //   description: "Located in the lush hills of Kandy district, Gerandi Falls is one of the most beautiful waterfalls in Sri Lanka, offering an adventurous journey through dense forests and rocky terrain.",
+    //   images: [require("assets/img/61.jpg")],
+    //   route: "/garadiElla"
+    // },
+    // {
+    //   title: "Yahangala Mountain",
+    //   description: "Steeped in Ramayana legend, Yahangala is the rock where King Ravana was laid to rest after he was killed by Lord Rama in the final battle. 'Yahangala' means bedrock in Sinhala.",
+    //   images: [require("assets/img/71.jpg")],
+    //   route: "/yahangala"
+    // },
+    // {
+    //   title: "Hanthana Mountain",
+    //   description: "The Hanthana Mountain Range lies in the central highlands of Sri Lanka, south-west of Kandy. The maximum height of the range is 1,200 m (3,800 ft). The mountain range consists of seven peaks.",
+    //   images: [require("assets/img/10.jpg")],
+    //   route: "/hanthana"
+    // },
+    // {
+    //   title: "Ambuluwawa",
+    //   description: "Ambuluwawa Tower is the first Multi-Religious Center in Sri Lanka. It is known that Ambuluwawa Temple is evocative of a Buddhist 'stupa' with a height of 48 meters.",
+    //   images: [require("assets/img/11.jpg")],
+    //   route: "/ambuluwawa"
+    // },
+    // {
+    //   title: "Udawaththa Kale",
+    //   description: "Udawattakele Forest Reserve often spelled as Udawatta Kele, is a historic forest reserve on a hill-ridge in the city of Kandy. It is 104 hectares large. During the days of the Kandyan kingdom, Udawattakele was known as 'Uda Wasala Watta' in Sinhalese meaning 'the garden above the royal palace'.",
+    //   images: [require("assets/img/55.jpg")],
+    //   route: "/udawaththakale"
+    // },
+    // {
+    //   title: "Peradeniya Campus",
+    //   description: "The University covers about 700 hectares of land and the developed area covering approximately 130 hectares is occupied by buildings of the Faculties, Halls of Residence, Staff bungalows etc. Throughout the campus most trees begin to bear flowers in early March signaling the coming of spring.",
+    //   images: [require("assets/img/76.jpg")],
+    //   route: "/peradeniyacampus"
+    // },
+    // {
+    //   title: "Bellwood",
+    //   description: "This magnificent scenic spot is not far from the Kandy city limits and you can easily reach the place from Kandy city via Ampitiya town to Talatuoya and then to Bellwood area. This place is considered a camping ground and if you camp overnight near Muthu Kelina lake make sure to watch the sunrise.",
+    //   images: [require("assets/img/33.jpg")],
+    //   route: "/bellwood"
+    // }
   ];
-  
