464855ff0747dfb5aab08ff19bb67507835dc861
Dilanjan <dilanjan@Dilanjans-MacBook-Air.local>
2024-07-16 22:07:52 +0530

    added video image to header1

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -1,21 +1,57 @@
 import React from "react";
 import { Container } from "reactstrap";
-
+import videoFile from "assets/video/v1.mp4";
 
 function IndexHeader() {
-
   return (
     <>
-      <div className="page-header clear-filter" id="home">
+      <div
+        className="page-header clear-filter"
+        id="home"
+        style={{
+          position: "relative",
+          height: "100vh",
+          overflow: "hidden",
+        }}
+      >
         <div
-          className="page-header-image"
           style={{
-            backgroundImage: "url(" + require("assets/video/v1.mp4") + ")",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            overflow: "hidden",
           }}
-
-        ></div>
+        >
+          <video
+            autoPlay
+            muted
+            loop
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              minWidth: "100%",
+              minHeight: "100%",
+              width: "auto",
+              height: "auto",
+              zIndex: -1,
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <source src={videoFile} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
         <Container>
-          <div className="content-center brand">
+          <div
+            className="content-center brand"
+            style={{
+              position: "relative",
+              zIndex: 1,
+            }}
+          >
             {/* <img
               alt="..."
               className="n-logo"
